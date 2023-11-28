4996eec3638488651907402f045771acf1df35b7
Florin Alexandru Sirbu <florinsirbu280599@gmail.com>
2023-11-28 09:57:18 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, Response, url_for
+import cv2
+
+app = Flask(__name__)
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)  # Change this to your screen capture device index
+
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    cap.release()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/streaming')
+def streaming():
+    return render_template('streaming.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
